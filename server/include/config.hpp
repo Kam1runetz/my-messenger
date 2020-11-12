5e43808e945757ea28a218be1a54886443d12b5f
Kam1runetz <notsoserious2017@gmail.com>
2020-11-12 06:46:27 +0300

    project sceleton created

--- server/include/config.hpp
+++ server/include/config.hpp
@@ -5,4 +5,6 @@
 #ifndef CRYPTO_MESSENGER_CONFIG_HPP
 #define CRYPTO_MESSENGER_CONFIG_HPP
 
+// todo: configuration file with general constants
+
 #endif //CRYPTO_MESSENGER_CONFIG_HPP
--- server/sources/source.cpp
+++ server/sources/source.cpp
@@ -2,3 +2,6 @@
 // Created by w1ckedente on 12.11.2020.
 //
 
+int main(int argc, char **argv) {
+    return 0;
+}
