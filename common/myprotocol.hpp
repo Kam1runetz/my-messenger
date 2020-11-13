ec4441ea2a75d37241d77e336c4c796e69377046
Kam1runetz <notsoserious2017@gmail.com>
2020-11-13 22:41:25 +0300

    project sceleton created

---
+++ common/myprotocol.hpp
@@ -0,0 +1,17 @@
+//
+// Created by w1ckedente on 13.11.2020.
+//
+
+#ifndef CRYPTO_MESSENGER_MYPROTOCOL_HPP
+#define CRYPTO_MESSENGER_MYPROTOCOL_HPP
+
+#include <cinttypes>
+#include <myprotocoldefs.hpp>
+
+#pragma pack(push, 1)
+
+// todo: myprotocol realisation
+
+#pragma pack(pop)
+
+#endif //CRYPTO_MESSENGER_MYPROTOCOL_HPP
---
+++ common/myprotocoldefs.hpp
@@ -0,0 +1,12 @@
+//
+// Created by w1ckedente on 13.11.2020.
+//
+
+#ifndef CRYPTO_MESSENGER_MYPROTOCOLDEFS_HPP
+#define CRYPTO_MESSENGER_MYPROTOCOLDEFS_HPP
+
+#include <cinttypes>
+
+// todo: myprotocol defs
+
+#endif //CRYPTO_MESSENGER_MYPROTOCOLDEFS_HPP
