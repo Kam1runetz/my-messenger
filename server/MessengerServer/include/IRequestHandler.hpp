e914e9a268b314f2a0c7fda6f09fa29ee0ffb1f5
Kam1runetz <notsoserious2017@gmail.com>
2020-11-16 22:34:51 +0300

    AppServer переименован в MessengerServer (более осмысленное название)

--- server/MessengerServer/include/IRequestHandler.hpp
+++ server/MessengerServer/include/IRequestHandler.hpp
@@ -1,7 +1,7 @@
 // Copyright 2020 <Kam1runetz notsoserious2017@gmail.com>
 
-#ifndef SERVER_REQUESTHANDLING_INCLUDE_IREQUESTHANDLER_HPP_
-#define SERVER_REQUESTHANDLING_INCLUDE_IREQUESTHANDLER_HPP_
+#ifndef SERVER_MESSENGERSERVER_INCLUDE_IREQUESTHANDLER_HPP_
+#define SERVER_MESSENGERSERVER_INCLUDE_IREQUESTHANDLER_HPP_
 
 #include <boost/asio/buffer.hpp>
 #include <myprotocol.hpp>
@@ -14,4 +14,4 @@ class IRequestHandler {
                                           MyProtocolPkg &response) = 0;
 };
 
-#endif  // SERVER_REQUESTHANDLING_INCLUDE_IREQUESTHANDLER_HPP_
+#endif  // SERVER_MESSENGERHANDLER_INCLUDE_IREQUESTHANDLER_HPP_
